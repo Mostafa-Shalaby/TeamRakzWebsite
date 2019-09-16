438c6290776197779d45ab346940d4609a7681ab
Mostafa Y Shalaby <mostafa.shalaby98@gmail.com>
2019-09-16 18:12:40 +0200

    Updates fontawesome, scss & adds contacts.
    - Change to webfont instead of svg.
    - Dynamic colored & fixed navigation menu.
    - Menu is now scrollable if it gets too long.
    - Moves Imports and variables to main.scss
    - Removes fontawesome script.
    - Cleans header.scss from any unused styling.
    -Removed most of the unused cover images
    -Compresses the coverpages in use
    -Finishs navigation structure.
    -Adds current active page highlighter.
    -Create links to all page, even if they dont exist.
    -Adds contacts page and its respective scss file.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,15 @@
 $(document).ready(function () {
     $('.loader').fadeOut('slow');
     $("#respMenu").aceResponsiveMenu({resizeWidth: "780" });
+    //Navigation Menu Background Changer
+    window.addEventListener('scroll', function() {
+        if (this.window.scrollY > 15) {
+            this.document.querySelector('nav').style.backgroundColor= 'rgba(51,51,51,1)';
+        } else {
+            this.document.querySelector('nav').style.backgroundColor= 'rgba(0,0,0,0)';
+        }
+    })
 });
-// //Navigation Menu Background Changer
-// window.addEventListener('scroll', function() {
-//     if (this.window.scrollY > 10) {
-//         this.document.querySelector('nav').style.backgroundColor= 'rgba(51,51,51,1)';
-//     } else {
-//         this.document.querySelector('nav').style.backgroundColor= 'rgba(0,0,0,0)';
-//     }
-// })
 
 
 
